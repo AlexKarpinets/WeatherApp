438187a033f73effdb4569bf7ce0e46363cfe374
AlexKarpinets <alexanderkarpinets@gmail.com>
2021-12-22 19:34:59 +0300

    Some Code

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -5,9 +5,13 @@
 //  Created by Karpinets Alexander on 20.12.2021.
 //
 
+import Alamofire
+import CoreLocation
+import NVActivityIndicatorView
+import SwiftyJSON
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var dayLabel: UILabel!
     @IBOutlet weak var conditionImageView: UIImageView!
@@ -17,13 +21,52 @@ class ViewController: UIViewController {
     
     private let gradientLayer = CAGradientLayer()
     
+    private let apiKey = "acbfe93e782f1a17ab4a98e10aee9b34"
+    private var lat = 62.424824
+    private var lon = 40.174954
+    private var activityIndicator: NVActivityIndicatorView!
+    private let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundView.layer.addSublayer(gradientLayer)
+        
+        let indicatorSize: CGFloat = 70
+        let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.width-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+        activityIndicator.backgroundColor = UIColor.black
+        view.addSubview(activityIndicator)
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        activityIndicator.startAnimating()
+        if(CLLocationManager.locationServicesEnabled()){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         setBlueBackground()
+    }
+    
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+      let location = locations[0]
+      lat = location.coordinate.latitude
+      lon = location.coordinate.longitude
+      
+      let request = AF.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric")
+      
+      request.responseDecodable { _ in
+          self.activityIndicator.stopAnimating()
+          if let responseStr = response.result.value {
+              let jsonResponse = JSON(responseStr)
+              let jsonWeather = jsonResponse["Weather"].array[0]
+              let jsonTemp = jsonResponse["main"]
+              let iconName = jsonWeather["icon"].stringValue
+          }
+      }
     }
     
     private func setBlueBackground() {
